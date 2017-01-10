c60d4336a2230716ab2701f6a2c730539fee01e6
chis <chis@oupeng.com>
2017-01-10 20:04:14 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,25 +3,25 @@
 var app = getApp()
 Page({
   data: {
-    activeTag: 'Hot',
-    hotList: [],
+    activeTab: 'hot',
+    refrushTxt: '上拉刷新',
+    loadmoreTxt: '下拉加载更多',
     userInfo: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
-
-  },
-  onLoad: function() {
-    console.log( 'onLoad' )
-    var _this = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo( function( userInfo ) {
-      //更新数据
-      _this.setData( {
-        userInfo: userInfo
-      })
+  //切换tab
+  bandleHotTab: function() {
+    this.setData({
+      activeTab: 'hot'
     })
-
+  },
+  handleCategoriesTab: function() {
+    this.setData({
+      activeTab: 'categories'
+    })
+  },
+  //请求初始数据
+  fetchInitData: function(cb) {
+    var _this = this
     //topList
     wx.request({
       url: 'http://123.57.21.57:8011/wxDemo/toplist.json',
@@ -31,10 +31,60 @@ Page({
         'Accept': 'application/json'
       },
       success: function(res) {
-        _this.hotList = res.data.data
-        console.log(_this.hotList)
+        console.log(res.data)
+        _this.setData({
+          hotList: res.data.data,
+          refrushTxt: '上拉刷新',
+        })
+        wx.stopPullDownRefresh()
       }
     })
-
+  },
+  // 请求更多数据
+  fetchMoreData: function() {
+    var _this = this
+    //topmoreList
+    wx.request({
+      url: 'http://123.57.21.57:8011/wxDemo/topmorelist.json',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function(res) {
+        var tmpList = [..._this.data.hotList, ...res.data.data]
+        _this.setData({
+          hotList: tmpList,
+          loadmoreTxt: '下拉加载更多',
+        })
+      }
+    })
+  },
+  onLoad: function() {
+    console.log( 'onLoad' )
+    var _this = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo( function( userInfo ) {
+      //更新数据
+      _this.setData({
+        userInfo: userInfo
+      })
+    })
+    this.fetchInitData()
+  },
+  // 上拉刷新
+  onPullDownRefresh: function() {
+    this.setData({
+      refrushTxt: '刷新中...',
+    })
+    setTimeout(() => {this.fetchInitData()}, 1000)
+    
+  },
+  // 下拉加载更多
+  onReachBottom: function () {
+    this.setData({
+      loadmoreTxt: '加载中...',
+    })
+    this.fetchMoreData()
   },
 })
